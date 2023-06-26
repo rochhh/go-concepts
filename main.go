e4a3593d2503e3e595b5fc4b12ef5c34124f9306
Roch <rochishnu7@gmail.com>
2023-06-27 00:14:33 +0530

    Sprintf

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, world!")
-	// this is a comment 
+    const name = "roch"
+	const age = 22
+
+	// const  msg = fmt.Sprintf("heya %v you're %v years old", name, age )
+	/* Cannot do the above as const with sprintf throws an err */
+	msg := fmt.Sprintf("heya %v you're %v years old", name, age )
+	
+
+	fmt.Println(msg)
 }
