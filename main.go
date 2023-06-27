4c1a91f6a0c842c1d0671017d26759ebd4d538de
Roch <rochishnu7@gmail.com>
2023-06-27 23:21:44 +0530

    what is type assertion ?

--- main.go
+++ main.go
@@ -4,30 +4,7 @@ import "fmt"
 
 func main() {
    
-   bk := corporate{
-      name: "burger king",
-      employees: 100000,
-   }
-
-   fmt.Println(bk.employees + bk.factoryClose())  
-
+   fmt.Println("What is type assertion?")
+   
 }
 
-
-func (c corporate ) factoryRunner() string {
-   return c.name
-}
-
-func (c corporate) factoryClose() int {
-   return 2
-}
-
-type corporate struct{
-   name string 
-   employees int 
-}
-
-type factory interface{
-   factoryRunner() string
-   factoryClose() 
-}
