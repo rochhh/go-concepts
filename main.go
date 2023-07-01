edf6c00da0402172d597d2fad90c8e6073626e74
Roch <rochishnu7@gmail.com>
2023-07-01 20:58:45 +0530

    try Q 3:23

--- main.go
+++ main.go
@@ -3,19 +3,7 @@ package main
 import "fmt"
 
 func main() {
-   
-   nums := [6]int{1,2,3,4,5,6}
-   numsSlices := nums[1:3]
-   
-   
-   // numsArray  := make( []int ,2 ,5  )
-   
-   fmt.Println(numsSlices )
-   // fmt.Println( do(nums[:]) )
+
+   // try Q ->  3:23
 
 }
-
-func do ( nums []int ) []int {
-   return nums
-}
-
