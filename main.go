6fff34e017f321c84145e726df3e955b00bba5e1
Roch <rochishnu7@gmail.com>
2023-07-01 20:41:17 +0530

    slices in go

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
    
-   mySlice := make( []int , 5 , 10 )
-   fmt.Println(mySlice)
+   nums := [6]int{1,2,3,4,5,6}
+   numsSlices := nums[1:3]
+   
+   
+   // numsArray  := make( []int ,2 ,5  )
+   
+   fmt.Println(numsSlices )
+   // fmt.Println( do(nums[:]) )
 
+}
 
+func do ( nums []int ) []int {
+   return nums
 }
 
