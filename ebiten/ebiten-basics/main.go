0b4211370608cf4e31e83878fb62c747142270c6
Roch <rochishnu7@gmail.com>
2023-11-03 21:12:17 +0530

    updates

---
+++ ebiten/ebiten-basics/main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"log"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image){
+	ebitenutil.DebugPrint(screen , "Hello World")
+}
+
+func (g *Game) Layout(outsideWidth , outsideHeight int) (screenWidth , screenHeight int){
+	return 320 , 240
+}
+
+func main() {
+	ebiten.SetWindowSize(720,540)
+	ebiten.SetWindowTitle("This is the title")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
